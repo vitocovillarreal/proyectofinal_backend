b33ee10546ab201f95189bd2a10969e302fb5317
vitocovillarreal <victor.villarrealj@gmail.com>
2023-09-10 12:03:33 -0300

    subida proyecto

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import express from "express"
+import productRouter from "../routes/products.router.js"
+import cartRouter from "../routes/cart.router.js"
+
+const app=express()
+const PORT=8080;
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+app.use(express.Router())
+app.use("/api",productRouter)
+app.use("/api",cartRouter)
+
+app.listen(PORT,()=>{
+    console.log("server is working")
+})
